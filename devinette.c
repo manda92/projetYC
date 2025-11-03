f3ff6f66575ea3cf69373ce87e4f59db62161585
Manda DABO <190718509+manda92@users.noreply.github.com>
2025-11-03 17:16:15 +0100

    fonc 2 manda

--- devinette.c
+++ devinette.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 
 int main() {
-	int nb_secret = 32;
-	int nb_user;
-	scanf("%d", &nb_user);
-	if (nb_user == nb_secret) {
-		printf("Succès");
-	} else {
-		printf("Echec");
-	}
-	return 0;
+    int nb_secret = 32;
+    int nb_user;
+
+    printf("Devine le nombre secret : ");
+    scanf("%d", &nb_user);
+
+    if (nb_user == nb_secret) {
+        printf("Succès !\n");
+    } else if (nb_user < nb_secret) {
+        printf("Trop petit !\n");
+    } else {
+        printf("Trop grand !\n");
+    }
+
+    return 0;
 }
